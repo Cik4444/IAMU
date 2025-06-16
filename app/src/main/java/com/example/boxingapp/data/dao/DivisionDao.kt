232cb865e8c94b83ae1c0c4223388d6c637844d9
Cik4444 <128796453+Cik4444@users.noreply.github.com>
2025-06-16 18:02:34 +0200

    Fix JvmSuppressWildcards usage

--- app/src/main/java/com/example/boxingapp/data/dao/DivisionDao.kt
+++ app/src/main/java/com/example/boxingapp/data/dao/DivisionDao.kt
@@ -3,6 +3,7 @@ package com.example.boxingapp.data.dao
 import androidx.room.*
 import com.example.boxingapp.data.entity.DivisionEntity
 import kotlinx.coroutines.flow.Flow
+import kotlin.jvm.JvmSuppressWildcards
 
 @Dao
 interface DivisionDao {
@@ -22,6 +23,7 @@ interface DivisionDao {
     @Query("SELECT id FROM divisions")
     suspend fun getAllIds(): List<String>
 
+    @JvmSuppressWildcards
     @Query("SELECT * FROM divisions WHERE id IN (:ids)")
     suspend fun getByIds(ids: List<String>): List<DivisionEntity>
 
